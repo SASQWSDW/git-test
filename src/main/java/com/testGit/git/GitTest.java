37e72acf9ae49f4285f482ec21696e451041e3ea
YIH <3475775055@qq.com>
2023-08-30 15:36:23 +0800

    hot commit

---
+++ src/main/java/com/testGit/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.testGit.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("second git");
+        System.out.println("master commit");
+    }
+}
