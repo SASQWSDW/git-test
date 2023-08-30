089f3a7c44dbca9906bc867d78fffd743647523d
YIH <3475775055@qq.com>
2023-08-30 15:48:30 +0800

    third commit

--- src/main/java/com/testGit/git/GitTest.java
+++ src/main/java/com/testGit/git/GitTest.java
@@ -2,7 +2,7 @@ package com.testGit.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Git");
+
         System.out.println("second git");
         System.out.println("master commit");
         System.out.println("third commit");
